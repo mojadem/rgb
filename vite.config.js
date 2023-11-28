961cc6bf8b7da98ddc6c4aa8f3ea9a52a76e28c8
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-11-28 01:05:22 -0500

    reverted vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import vercel from "vite-plugin-vercel";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [vercel(), glsl()],
+  plugins: [glsl()],
 });

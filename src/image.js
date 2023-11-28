bb3a592fad115e977d88948c1c10232cea01b57e
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-11-28 15:49:10 -0500

    changed to free api

--- src/image.js
+++ src/image.js
@@ -4,19 +4,13 @@ import * as THREE from "three";
  * @param {THREE.Mesh[]} planes
  */
 async function loadTexture(planes) {
-  fetch("http://localhost:3000/api/get-image", {
-    headers: { Accept: "image/jpg" },
-  })
-    .then((response) => response.blob())
-    .then((textureBlob) => {
-      const objectURL = URL.createObjectURL(textureBlob);
-      const texture = new THREE.TextureLoader().load(objectURL);
+  const response = await fetch("https://picsum.photos/500/500");
+  const texture = new THREE.TextureLoader().load(response.url);
 
-      for (const plane of planes) {
-        plane.material.uniforms.u_texture.value = texture;
-        plane.material.needsUpdate = true;
-      }
-    });
+  for (let plane of planes) {
+    plane.material.uniforms.u_texture.value = texture;
+    plane.material.needsUpdate = true;
+  }
 }
 
 export { loadTexture };

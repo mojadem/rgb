027a4383f8ede643da055ee75eb4f8cbc19a314f
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-12-07 12:29:17 -0500

    added image refreshing

--- src/gui.js
+++ src/gui.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { updateTint } from "./planes";
+import { getImage } from "./image";
 
 /** @type {Element} */
 let redSlider;
@@ -7,6 +8,8 @@ let redSlider;
 let greenSlider;
 /** @type {Element} */
 let blueSlider;
+/** @type {Element} */
+let button;
 /** @type {THREE.Color} */
 const tint = new THREE.Color(0.0, 0.0, 0.0);
 
@@ -29,6 +32,16 @@ function initGui() {
   blueSlider.value = tint.b;
   blueSlider.oninput = () => {
     tint.b = parseFloat(blueSlider.value);
+    updateTint(tint);
+  };
+
+  button = document.getElementById("button");
+  button.onclick = () => {
+    getImage();
+    tint.copy(new THREE.Color(0.0, 0.0, 0.0));
+    redSlider.value = tint.r;
+    greenSlider.value = tint.g;
+    blueSlider.value = tint.b;
     updateTint(tint);
   };
 }
